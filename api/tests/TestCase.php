b2e95089de6abd657e2a804251198596c7e1cb20
José Airton Feitoza <airton.feitoza@midtec.net.br>
2021-04-11 16:52:49 -0300

    Inserindo stubs no gitignore.

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -7,4 +7,21 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $model;
+
+    public function dataFaker($type, $model, int $amoutOfFaker = null, array $attributes = [])
+    {
+        $this->model = $this->resolveModel($model);
+        if ($type == 'create') {
+            return $this->model->factory($amoutOfFaker)->create($attributes);
+        }else{
+            return $this->model->factory($amoutOfFaker)->make($attributes);
+        }
+    }
+
+    protected function resolveModel($model)
+    {
+        return app($model);
+    }
 }
